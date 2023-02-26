fd078a11024d163c467c730c6bcc82e94a09b6f9
Tom Jemmett <tom.jemmett@gmail.com>
2023-02-26 22:31:46 +0000

    day 01 solution

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,6 +1,10 @@
-module Day01 where -- (day01) where
+module Day01 (day01) where
 
 import Common
+import Data.Char (digitToInt)
 
 day01 :: AOCSolution
-day01 input = []
+day01 input = go <$> [1, length input `div` 2] <*> pure (head $ lines input)
+
+go :: Int -> String -> String
+go n i = show . sum . map (digitToInt . fst) . filter (uncurry (==)) $ zip i $ drop n $ cycle i
--- test/Day01Spec.hs
+++ test/Day01Spec.hs
@@ -2,18 +2,13 @@ module Day01Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
-
 spec :: Spec
 spec = describe "Day 1" $ do
-  it "Sample" $ do
-    day01 testInput `shouldBe` []
-
   it "Actual" $ do
     withFile
       "inputs/day01.txt"
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day01 actualInput `shouldBe` []
+          day01 actualInput `shouldBe` ["1089", "1156"]
       )
