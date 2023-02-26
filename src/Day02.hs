48afce77fe9f05ca05697d4ef567ced30fa01f15
Tom Jemmett <tom.jemmett@gmail.com>
2023-02-26 22:37:00 +0000

    day 02 solution

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,6 +1,20 @@
-module Day02 where -- (day02) where
+module Day02 (day02) where
 
 import Common
+import Data.List (sort)
 
 day02 :: AOCSolution
-day02 input = []
+day02 input = go <$> [p1, p2] <*> pure (parseInput input)
+
+parseInput :: String -> [[Int]]
+parseInput = map (reverse . sort . map read . words) . lines
+
+go :: ([Int] -> Int) -> [[Int]] -> String
+go f = show . sum . map f
+
+p1 :: [Int] -> Int
+p1 i = head i - last i
+
+p2 :: [Int] -> Int
+p2 [] = 0
+p2 (x : xs) = p2 xs + sum (map fst $ filter ((== 0) . snd) $ map (divMod x) xs)
