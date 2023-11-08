38ac60f44cf2a0729a00f8041c684de61c002a06
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-08 19:45:37 +0000

    adds day 6 solution

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,6 +1,23 @@
 module Day06 where -- (day06) where
 
 import Common
+import Data.HashMap.Strict qualified as M
 
 day06 :: AOCSolution
-day06 input = []
+day06 = go M.empty 0 . wordSeparatedInts
+
+go :: M.HashMap [Int] Int -> Int -> [Int] -> [String]
+go h n b =
+  case M.lookup b h of
+    Just j -> show <$> [n, n - j]
+    Nothing -> go (M.insert b n h) (succ n) nb
+      where
+        nb = f $ span (maximum b /=) b
+        --
+        f :: ([Int], [Int]) -> [Int]
+        f (x, y : z) = g y (0 : reverse x) z
+        --
+        g :: Int -> [Int] -> [Int] -> [Int]
+        g 0 x y = reverse x ++ y
+        g i x [] = g i [] (reverse x)
+        g i x (y : ys) = g (i - 1) ((y + 1) : x) ys
--- test/Day06Spec.hs
+++ test/Day06Spec.hs
@@ -2,12 +2,12 @@ module Day06Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "0\t2\t7\t0"
 
 spec :: Spec
 spec = describe "Day 6" $ do
   it "Sample" $ do
-    day06 testInput `shouldBe` []
+    day06 testInput `shouldBe` ["5", "4"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +15,5 @@ spec = describe "Day 6" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day06 actualInput `shouldBe` []
+          day06 actualInput `shouldBe` ["6681", "2392"]
       )
