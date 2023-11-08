bbd066917ebe17a567ab09509c9b477ac66a5b8f
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-08 09:43:16 +0000

    adds day 4 solution

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,6 +1,13 @@
-module Day04 where -- (day04) where
+module Day04 (day04) where
 
 import Common
+import Data.List (group, sort)
 
 day04 :: AOCSolution
-day04 input = []
+day04 input = run <$> [id, map sort] <*> pure input
+
+run :: ([String] -> [String]) -> String -> String
+run fn = show . countTrue id . map go . lines
+  where
+    go :: String -> Bool
+    go = all ((== 1) . length) . group . sort . fn . words
--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -2,12 +2,20 @@ module Day04Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "aa bb cc dd ee\n\
+  \aa bb cc dd aa\n\
+  \aa bb cc dd aaa\n\
+  \abcde fghij\n\
+  \abcde xyz ecdab\n\
+  \a ab abc abd abf abj\n\
+  \iiii oiii ooii oooi oooo\n\
+  \oiii ioii iioi iiio"
 
 spec :: Spec
 spec = describe "Day 4" $ do
   it "Sample" $ do
-    day04 testInput `shouldBe` []
+    day04 testInput `shouldBe` ["7", "5"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +23,5 @@ spec = describe "Day 4" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day04 actualInput `shouldBe` []
+          day04 actualInput `shouldBe` ["337", "231"]
       )
