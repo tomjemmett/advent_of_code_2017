261d3b26ad9f98a75b3c72125d959b4a87d12bef
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-08 10:25:17 +0000

    adds day 5 solution

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,6 +1,23 @@
-module Day05 where -- (day05) where
+module Day05 (day05) where
 
 import Common
+import Control.Monad.ST (runST)
+import Data.HashMap.Strict qualified as H
+import Data.Vector.Unboxed qualified as V
+import Data.Vector.Unboxed.Mutable qualified as MV
 
 day05 :: AOCSolution
-day05 input = []
+day05 input = show . go v <$> [succ, p2]
+  where
+    v = V.fromList . map read . lines $ input
+    p2 v = (if v >= 3 then pred else succ) v
+
+go :: V.Vector Int -> (Int -> Int) -> Int
+go v fn = runST $ V.thaw v >>= step 0 0
+  where
+    step n i vm
+      | i < 0 || i >= MV.length vm = return n
+      | otherwise = do
+          val <- MV.read vm i
+          MV.write vm i $ fn val
+          step (succ n) (i + val) vm
--- test/Day05Spec.hs
+++ test/Day05Spec.hs
@@ -2,12 +2,12 @@ module Day05Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "0\n3\n0\n1\n-3"
 
 spec :: Spec
 spec = describe "Day 5" $ do
   it "Sample" $ do
-    day05 testInput `shouldBe` []
+    day05 testInput `shouldBe` ["5", "10"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +15,5 @@ spec = describe "Day 5" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day05 actualInput `shouldBe` []
+          day05 actualInput `shouldBe` ["372671", "25608480"]
       )
