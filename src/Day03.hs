f7eac00096ef944e86469b60af61a0e64973f608
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-08 09:42:57 +0000

    adds day 3 solution

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,6 +1,37 @@
-module Day03 where -- (day03) where
+module Day03 (day03) where
 
 import Common
+import Data.Bifunctor (Bifunctor, first, second)
+import Data.Function ((&))
+import Data.HashMap.Strict qualified as H
 
 day03 :: AOCSolution
-day03 input = []
+day03 input = show <$> ([part1, part2] <*> pure (read input))
+
+part1 :: Integer -> Integer
+part1 i = (t - 1) - (t * t - i)
+  where
+    t = toInteger . ceiling . sqrt . fromIntegral $ i
+
+part2 :: Integer -> Integer
+part2 i = head $ dropWhile (< i) $ 1 : fn positions init
+  where
+    init :: H.HashMap Point2d Integer
+    init = H.singleton (0, 0) 1
+    fn :: [Point2d] -> H.HashMap Point2d Integer -> [Integer]
+    fn (p : ps) m = v : fn ps (H.insert p v m)
+      where
+        v = foldl (\v i -> v + H.lookupDefault 0 i m) 0 . point2dNeighboursDiags $ p
+    positions :: [Point2d]
+    positions = tail $ scanl (&) (0, 0) $ concat $ zipWith replicate s d
+      where
+        s :: [Int]
+        s = concatMap (\a -> [a, a]) [1 ..]
+        d :: [Point2d -> Point2d]
+        d =
+          cycle
+            [ first succ, -- right
+              second succ, -- up
+              first pred, -- left
+              second pred -- right
+            ]
--- test/Day03Spec.hs
+++ test/Day03Spec.hs
@@ -2,12 +2,12 @@ module Day03Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput = "1024"
 
 spec :: Spec
 spec = describe "Day 3" $ do
   it "Sample" $ do
-    day03 testInput `shouldBe` []
+    day03 testInput `shouldBe` ["31", "1968"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +15,5 @@ spec = describe "Day 3" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day03 actualInput `shouldBe` []
+          day03 actualInput `shouldBe` ["438", "266330"]
       )
