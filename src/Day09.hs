f0dc02a52e6df57fe25f582b945c06546d2da82b
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-09 21:12:09 +0000

    adds day 9 solution

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,6 +1,23 @@
-module Day09 where -- (day09) where
+module Day09 (day09) where
 
 import Common
 
 day09 :: AOCSolution
-day09 input = []
+day09 = f 0 0 0
+  where
+    f :: Int -> Int -> Int -> String -> [String]
+    f lvl p1 p2 [] = show <$> [p1, p2]
+    f lvl p1 p2 (x : xs) = case x of
+      '!' -> f lvl p1 p2 $ tail xs
+      '<' -> f lvl p1 d' xs'
+      '{' -> f (lvl + 1) (p1 + lvl + 1) p2 xs
+      '}' -> f (lvl - 1) p1 p2 xs
+      _ -> f lvl p1 p2 xs
+      where
+        (d', xs') = g p2 xs
+    --
+    g :: Int -> String -> (Int, String)
+    g p2 (x : xs) = case x of
+      '!' -> g p2 $ tail xs
+      '>' -> (p2, xs)
+      _ -> g (p2 + 1) xs
--- test/Day09Spec.hs
+++ test/Day09Spec.hs
@@ -2,18 +2,13 @@ module Day09Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
-
 spec :: Spec
 spec = describe "Day 9" $ do
-  it "Sample" $ do
-    day09 testInput `shouldBe` []
-
   it "Actual" $ do
     withFile
       "inputs/day09.txt"
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day09 actualInput `shouldBe` []
+          day09 actualInput `shouldBe` ["14212", "6569"]
       )
