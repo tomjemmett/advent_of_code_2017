a6f769a261f2792c83cc942d83bfdd864011b93e
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-10 14:47:38 +0000

    adds day 12 solution

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,6 +1,40 @@
-module Day12 where -- (day12) where
+module Day12 (day12) where
 
 import Common
+import Control.Monad.RWS (MonadWriter (pass))
+import Data.HashMap.Strict qualified as M
+import Data.HashSet qualified as S
+import Text.Parsec qualified as P
 
 day12 :: AOCSolution
-day12 input = []
+day12 input = show <$> [p1, p2]
+  where
+    i = parseInput input
+    p1 = S.size $ go i S.empty [0]
+    p2 = groups i (M.keys i) 0
+
+groups :: M.HashMap Int (S.HashSet Int) -> [Int] -> Int -> Int
+groups _ [] g = g
+groups m (x : xs) g = groups m xs' (g + 1)
+  where
+    a = go m S.empty [x]
+    xs' = S.toList $ S.difference (S.fromList xs) a
+
+go :: M.HashMap Int (S.HashSet Int) -> S.HashSet Int -> [Int] -> S.HashSet Int
+go _ s [] = s
+go m s (i : is) = go m s' (S.toList n ++ is)
+  where
+    n = m M.! i `S.difference` s
+    s' = S.insert i s
+
+parseInput :: String -> M.HashMap Int (S.HashSet Int)
+parseInput = M.fromList . map parseLine . lines
+
+parseLine :: String -> (Int, S.HashSet Int)
+parseLine = parse' p id
+  where
+    p = do
+      a <- number
+      P.string " <-> "
+      b <- number `P.sepBy` P.string ", "
+      return (a, S.fromList b)
--- test/Day12Spec.hs
+++ test/Day12Spec.hs
@@ -2,12 +2,19 @@ module Day12Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "0 <-> 2\n\
+  \1 <-> 1\n\
+  \2 <-> 0, 3, 4\n\
+  \3 <-> 2, 4\n\
+  \4 <-> 2, 3, 6\n\
+  \5 <-> 6\n\
+  \6 <-> 4, 5"
 
 spec :: Spec
 spec = describe "Day 12" $ do
   it "Sample" $ do
-    day12 testInput `shouldBe` []
+    day12 testInput `shouldBe` ["6", "2"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +22,5 @@ spec = describe "Day 12" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day12 actualInput `shouldBe` []
+          day12 actualInput `shouldBe` ["141", "171"]
       )
