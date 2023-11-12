18fa4eb90773adcecd85f3623480f91042ec5f86
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-12 21:31:14 +0000

    adds day 17 solution

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,6 +1,26 @@
-module Day17 where -- (day17) where
+module Day17 (day17) where
 
 import Common
+import Data.List (splitAt)
 
 day17 :: AOCSolution
-day17 input = []
+day17 input = show <$> ([part1, part2] <*> pure (read input))
+
+part1 :: Int -> Int
+part1 s = f 1 1 [0]
+  where
+    f 2018 p xs = xs !! succ p
+    f n p xs = f (succ n) p' xs'
+      where
+        !p' = (p + s `mod` n + 1) `mod` n
+        (a, b) = splitAt p' xs
+        !xs' = a ++ n : b
+
+part2 :: Int -> Int
+part2 s = f 1 1 1
+  where
+    f 50000001 _ v = v
+    f n p v = f (succ n) p' v'
+      where
+        !p' = (p + s `mod` n + 1) `mod` n
+        !v' = if p' == 0 then n else v
--- test/Day17Spec.hs
+++ test/Day17Spec.hs
@@ -1,19 +1,26 @@
-module Day17Spec (spec) where
+module Day17 (day17) where
 
-import SpecHelper
+import Common
+import Data.List (splitAt)
 
-testInput = ""
+day17 :: AOCSolution
+day17 input = show <$> ([part1, part2] <*> pure (read input))
 
-spec :: Spec
-spec = describe "Day 17" $ do
-  it "Sample" $ do
-    day17 testInput `shouldBe` []
+part1 :: Int -> Int
+part1 s = f 1 1 [0]
+  where
+    f 2018 p xs = xs !! succ p
+    f n p xs = f (succ n) p' xs'
+      where
+        !p' = (p + s `mod` n + 1) `mod` n
+        (a, b) = splitAt p' xs
+        !xs' = a ++ n : b
 
-  it "Actual" $ do
-    withFile
-      "inputs/day17.txt"
-      ReadMode
-      ( \h -> do
-          actualInput <- hGetContents h
-          day17 actualInput `shouldBe` []
-      )
+part2 :: Int -> Int
+part2 s = f 1 1 1
+  where
+    f 50000001 _ v = v
+    f n p v = f (succ n) p' v'
+      where
+        !p' = (p + s `mod` n + 1) `mod` n
+        !v' = if p' == 0 then n else v
