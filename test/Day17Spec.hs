dd7d37f04beae1b4928e081a4a9354d4e201a9cc
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-12 21:45:18 +0000

    fixes day 17

--- test/Day17Spec.hs
+++ test/Day17Spec.hs
@@ -1,26 +1,19 @@
-module Day17 (day17) where
+module Day17Spec (spec) where
 
-import Common
-import Data.List (splitAt)
+import SpecHelper
 
-day17 :: AOCSolution
-day17 input = show <$> ([part1, part2] <*> pure (read input))
+testInput = "3"
 
-part1 :: Int -> Int
-part1 s = f 1 1 [0]
-  where
-    f 2018 p xs = xs !! succ p
-    f n p xs = f (succ n) p' xs'
-      where
-        !p' = (p + s `mod` n + 1) `mod` n
-        (a, b) = splitAt p' xs
-        !xs' = a ++ n : b
+spec :: Spec
+spec = describe "Day 17" $ do
+  it "Sample" $ do
+    day17 testInput `shouldBe` ["638", "1222153"]
 
-part2 :: Int -> Int
-part2 s = f 1 1 1
-  where
-    f 50000001 _ v = v
-    f n p v = f (succ n) p' v'
-      where
-        !p' = (p + s `mod` n + 1) `mod` n
-        !v' = if p' == 0 then n else v
+  it "Actual" $ do
+    withFile
+      "inputs/day17.txt"
+      ReadMode
+      ( \h -> do
+          actualInput <- hGetContents h
+          day17 actualInput `shouldBe` ["725", "27361412"]
+      )
