29ae5bef6a8f7edeed3e5755cadb04148fc5df89
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-15 21:55:46 +0000

    adds day 23 solution

--- src/Day23.hs
+++ src/Day23.hs
@@ -1,8 +1,20 @@
 {-# OPTIONS_GHC -Wno-x-partial #-}
 
-module Day23 where -- (day23) where
+module Day23 (day23) where
 
 import Common
+import Data.List.Split (chunksOf)
+import Data.Numbers.Primes (isPrime)
 
 day23 :: AOCSolution
-day23 input = []
+day23 input = show <$> ([part1, part2] <*> pure x)
+  where
+    x = read $ last $ words $ head $ lines input
+
+part1 :: Int -> Int
+part1 x = (x - 2) * (x - 2)
+
+part2 :: Int -> Int
+part2 x = length $ filter (not . isPrime . head) $ chunksOf 17 [x' .. x' + 17000]
+  where
+    x' = x * 100 + 100000
