af57682a7de37ac3d933d7d74e7f1f2d592d5a39
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-16 15:44:15 +0000

    adds day 25 solution

--- src/Day25.hs
+++ src/Day25.hs
@@ -3,6 +3,56 @@
 module Day25 where -- (day25) where
 
 import Common
+import Data.Char (digitToInt)
+import Data.List (splitAt)
+import Data.HashMap.Strict qualified as M
+
+type Program = M.HashMap Char [(Int, Int, Char)]
+type Tape = M.HashMap Int Int
 
 day25 :: AOCSolution
-day25 input = []
+day25 input = show <$> [p1]
+  where
+    (s, d, i) = parseInput input
+    p1 = go i s 0 d M.empty
+
+go :: Program -> Char -> Int -> Int -> Tape -> Int
+go _ _ _ 0 t = sum $ M.elems t
+go i s p d t = go i s' p' d' t'
+  where
+    v = M.lookupDefault 0 p t
+    (a, b, s') = (i M.! s) !! v
+    t' = M.insert p a t
+    p' = p + b
+    d' = pred d
+
+
+parseInput :: String -> (Char, Int, Program)
+parseInput input = (i, d, s)
+  where
+    (initState : diagnostic : states) = lines input
+    i = initState !! 15
+    d = read $ (words diagnostic) !! 5
+    s = M.fromList $ parseStates states
+
+parseStates :: [String] -> [(Char, [(Int, Int, Char)])]
+parseStates [] = []
+parseStates xs = parseState x : parseStates xs'
+  where
+    (x, xs') = splitAt 10 xs
+
+parseState :: [String] -> (Char, [(Int, Int, Char)])
+parseState (_ : s : sb) = (s !! 9, [f a, f b])
+  where
+    (a, b) = splitAt 4 sb
+    f [_, x, y, z] = (px x, py y, pz z)
+    px :: String -> Int
+    px (drop 22 -> x : _) = digitToInt x
+    py :: String -> Int
+    py = \case
+      "    - Move one slot to the right." -> 1
+      _ -> -1
+    pz :: String -> Char
+    pz (drop 26 -> z : _) = z
+
+getI = parseInput <$> readFile "inputs/day25.txt"
--- test/Day25Spec.hs
+++ test/Day25Spec.hs
@@ -2,12 +2,34 @@ module Day25Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "Begin in state A.\n\
+  \Perform a diagnostic checksum after 6 steps.\n\
+  \\n\
+  \In state A:\n\
+  \  If the current value is 0:\n\
+  \    - Write the value 1.\n\
+  \    - Move one slot to the right.\n\
+  \    - Continue with state B.\n\
+  \  If the current value is 1:\n\
+  \    - Write the value 0.\n\
+  \    - Move one slot to the left.\n\
+  \    - Continue with state B.\n\
+  \\n\
+  \In state B:\n\
+  \  If the current value is 0:\n\
+  \    - Write the value 1.\n\
+  \    - Move one slot to the left.\n\
+  \    - Continue with state A.\n\
+  \  If the current value is 1:\n\
+  \    - Write the value 1.\n\
+  \    - Move one slot to the right.\n\
+  \    - Continue with state A."
 
 spec :: Spec
 spec = describe "Day 25" $ do
   it "Sample" $ do
-    day25 testInput `shouldBe` []
+    day25 testInput `shouldBe` ["3"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +37,5 @@ spec = describe "Day 25" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day25 actualInput `shouldBe` []
+          day25 actualInput `shouldBe` ["3578"]
       )
