968f9776ba16369f3026fcf29272e2cdda2eb97d
Tom Jemmett <tom.jemmett@gmail.com>
2023-11-16 15:03:51 +0000

    adds day 24 solution

--- src/Day24.hs
+++ src/Day24.hs
@@ -1,8 +1,39 @@
 {-# OPTIONS_GHC -Wno-x-partial #-}
 
-module Day24 where -- (day24) where
+module Day24 (day24) where
 
 import Common
+import Control.Applicative (Alternative (..))
+import Control.Monad.Trans.State (StateT (..), evalStateT)
+import Data.Bifunctor (first)
+import Data.List.Split (splitOn)
+import Data.Ord (Down (..))
+import Data.Tuple (swap)
+
+type Comp = (Int, Int)
 
 day24 :: AOCSolution
-day24 input = []
+day24 input = show <$> [p1, p2]
+  where
+    i = parseInput input
+    b = runStateT (bridge 0) i
+    p1 = maximum $ fst <$> b
+    p2 = snd . maximum $ first (Down . length) . swap <$> b
+
+parseInput :: String -> [Comp]
+parseInput = map (tuplify2 . map read . splitOn "/") . lines
+
+select :: [a] -> [(a, [a])]
+select [] = []
+select (x : xs) = (x, xs) : [(y, x : ys) | (y, ys) <- select xs]
+
+bridge :: Int -> StateT [Comp] [] Int
+bridge frm = do
+  (x, y) <- StateT select
+  next <-
+    if
+        | x == frm -> return y
+        | y == frm -> return x
+        | otherwise -> empty
+  rest <- return 0 <|> bridge next
+  return $ x + y + rest
--- test/Day24Spec.hs
+++ test/Day24Spec.hs
@@ -2,12 +2,20 @@ module Day24Spec (spec) where
 
 import SpecHelper
 
-testInput = ""
+testInput =
+  "0/2\n\
+  \2/2\n\
+  \2/3\n\
+  \3/4\n\
+  \3/5\n\
+  \0/1\n\
+  \10/1\n\
+  \9/10"
 
 spec :: Spec
 spec = describe "Day 24" $ do
   it "Sample" $ do
-    day24 testInput `shouldBe` []
+    day24 testInput `shouldBe` ["31", "19"]
 
   it "Actual" $ do
     withFile
@@ -15,5 +23,5 @@ spec = describe "Day 24" $ do
       ReadMode
       ( \h -> do
           actualInput <- hGetContents h
-          day24 actualInput `shouldBe` []
+          day24 actualInput `shouldBe` ["1511", "1471"]
       )
